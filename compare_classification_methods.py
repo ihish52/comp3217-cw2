bdd36db99deee2920778f0cdd68a6113da363432
ihish52 <ihish52@gmail.com>
2021-05-07 12:54:44 +0100

    minor changes to printing text

--- compare_classification_methods.py
+++ compare_classification_methods.py
@@ -43,41 +43,41 @@ neigh.fit(x_train, y_train)
 y_pred = neigh.predict(x_test)
 y_pred_train = neigh.predict(x_train_full_2)
 print("\nAccuracy for KNN on Training Set:",metrics.accuracy_score(y_train_full, y_pred_train))
-print("Accuracy for KNN on 25% Testing Set:",metrics.accuracy_score(y_test, y_pred))
+print("Accuracy for KNN on 20% Testing Set:",metrics.accuracy_score(y_test, y_pred))
 
 #Decision Tree
 tree = DecisionTreeClassifier().fit(x_train, y_train)
 print("\nAccuracy for Decision Tree classifier on Training set:",tree.score(x_train_full_2, y_train_full))
-print("Accuracy for Decision Tree classifier on 25% Testing set:",tree.score(x_test, y_test))
+print("Accuracy for Decision Tree classifier on 20% Testing set:",tree.score(x_test, y_test))
 
 #Logistic Regression
 log = linear_model.LogisticRegression(random_state = 2,solver = 'liblinear',multi_class = 'auto')
 log = log.fit(x_train, y_train)
 print("\nAccuracy for Logistic Regression on Training set:",log.score(x_train_full_2, y_train_full))
-print("Accuracy for Logistic Regression on 25% Testing set:",log.score(x_test, y_test))
+print("Accuracy for Logistic Regression on 20% Testing set:",log.score(x_test, y_test))
 
 #Support Vector Machine
 svm = SVC()
 svm.fit(x_train, y_train)
 #print("\nAccuracy for SVM classifier on 75% Training set:",svm.score(x_train, y_train))
 print("\nAccuracy for SVM classifier on Training set:",svm.score(x_train_full_2, y_train_full))
-print("Accuracy for SVM classifier on 25% Testing set:",svm.score(x_test, y_test))
+print("Accuracy for SVM classifier on 20% Testing set:",svm.score(x_test, y_test))
 
 #Linear Discriminant Analysis
 lda = LinearDiscriminantAnalysis()
 lda.fit(x_train, y_train)
 print("\nAccuracy for LDA classifier on Training set:",lda.score(x_train_full_2, y_train_full))
-print("Accuracy for LDA classifier on 25% Testing set:",lda.score(x_test, y_test))
+print("Accuracy for LDA classifier on 20% Testing set:",lda.score(x_test, y_test))
 
 #Gaussian Naive Bayes
 gnb = GaussianNB()
 gnb.fit(x_train, y_train)
 print("\nAccuracy for GNB classifier on Training set:",gnb.score(x_train_full_2, y_train_full))
-print("Accuracy for GNB classifier on 25% Testing set:",gnb.score(x_test, y_test))
+print("Accuracy for GNB classifier on 20% Testing set:",gnb.score(x_test, y_test))
 
 #Multi Layer Perception
 reg = MLPRegressor(hidden_layer_sizes=(64,64,64),activation="relu" ,random_state=1, max_iter=2000).fit(x_train, y_train)
 y_pred_mlp=reg.predict(x_test)
 y_pred_mlp_train=reg.predict(x_train_full_2)
 print("\nAccuracy for MLP on Training Set:",r2_score(y_pred_mlp_train, y_train_full))
-print("Accuracy for MLP on 25% Testing Set:",r2_score(y_pred_mlp, y_test))
+print("Accuracy for MLP on 20% Testing Set:",r2_score(y_pred_mlp, y_test))
